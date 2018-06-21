e25ff18c60f4efea6babb86fbd339d207dc0a83b
Dileep Singh <dileep2008apex@gmail.com>
2018-06-21 12:33:19 +0530

    Update eg.ts

--- eg.ts
+++ eg.ts
@@ -305,3 +305,25 @@ class Employee extends Person {
 let howard = new Employee("Howard", "Sales");
 console.log(howard.getElevatorPitch());
 console.log(howard.name); // Error
+--------------------------
+ class Person {
+    protected name : string;
+    protected constructor(theName : string) {
+        this.name = theName;
+    }
+}
+
+class Employee extends Person {
+    private department : string;
+    constructor(name : string, department : string) {
+        super(name);
+        this.department = department;
+    }
+     
+  public getElevatorPitch() {
+      return `Hello, my name is ${this.name} and I work in ${this.department}.`;
+  }
+}
+
+let howard = new Employee("Howard", "Sales");
+let john = new Person("John");// Error:  Constructor of class 'Person' is protected and only accessible within the class declaration.
